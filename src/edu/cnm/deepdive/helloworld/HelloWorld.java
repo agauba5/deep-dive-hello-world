9d76b273dbdec2292c8c20a562bc02baa88d848f
Abdul Haseeb Gauba <abdul.h.gauba5@gmail.com>
2017-09-04 17:05:09 -0600

    New Project files, with fiel implementing "Hello World!" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author Abdul Haseeb Gauba
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * Displays "Hello World!" message
+   */
+  public static void main(String[] args) {
+  System.out.println("Hello World!");  
+  }
+
+}
